78e150e7bc61823e46e3cb508c850992edec42cb
linrenwei <windstreet12@gmail.com>
2019-03-22 11:16:01 +0800

    7

--- learning_record/rec_marshmallow/run_08_nesting.py
+++ learning_record/rec_marshmallow/run_08_nesting.py
@@ -5,28 +5,14 @@
 1）当模型含有外键，那这个外键的对象在Schemas如何定义。
 
 
-2）Specifying Deserialization Keys（ Schema 的 field 名称定义成一样，但反序列化成不同的名字 ）
-Schemas默认会 反序列化 传入字典和输出字典中相同的字段名。
-如果你觉得数据不匹配你的schema，你可以传入 load_from 参数指定需要增加load的字段名（原字段名也能load，且 优先load原字段名）：
-
-
-3）Specifying Serialization Keys（ Schema 的 field 名称定义成一样，但反序列化成不同的名字 ）
-如果你需要序列化一个 field 成一个不同的名字时，可以使用 dump_to，逻辑和 load_from 类似。
-
-
-
-
-如果field 是多个对象的集合，定义时可以使用many参数:
+2）如果field 是多个对象的集合，定义时可以使用 many 参数:
 collaborators = fields.Nested(UserSchema, many=True)
 
-如果外键对象是自引用，则Nested里第一个参数为'self'
 
-
-
-
-
+3）如果外键对象是自引用，则Nested里第一个参数为 'self'
 
 """
+
 from marshmallow import Schema, fields, pprint
 import datetime as dt
 
@@ -55,7 +41,9 @@ class UserSchema(Schema):
 
 class BlogSchema(Schema):
     title = fields.String()
-    author = fields.Nested(UserSchema)  # 使用 Nested field 表示外键对象：
+    author = fields.Nested(UserSchema)  # 使用 Nested field 表示外键对象。
+    # collaborators = fields.Nested(UserSchema, many=True)
+    # same_blog = fields.Nested("self", many=True)
 
 
 if __name__ == "__main__":
