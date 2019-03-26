74f39209d6ac8110db75db0289d305ecf1cba871
linrenwei <windstreet12@gmail.com>
2019-03-26 20:18:12 +0800

    19

--- learning_record/rec_sqlalchemy/sqlalchemy_tutorial/S01_connection.py
+++ learning_record/rec_sqlalchemy/sqlalchemy_tutorial/S01_connection.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """connection
+
     使用传统的connection的方式连接和操作数据库;
     与python自带的sqlite不同，这里不需要Cursor光标，执行sql语句不需要commit.
 """
@@ -9,22 +10,31 @@ from sqlalchemy import create_engine
 
 
 # 数据库连接字符串
-DB_CONNECT_STRING = "{drivername}://{user}:{passwd}@{host}:{port}/{db_name}".format(
-    drivername="postgresql+psycopg2",  # default: postgresql	# psycopg2: postgresql+psycopg2
-    user="admin",
-    passwd="",
-    host="localhost",
-    port="5432",
-    db_name="admin",
-)
+# DB_CONNECT_STRING = "{driver_name}://{user}:{password}@{host}:{port}/{db_name}".format(
+#     driver_name="postgresql+psycopg2",  # default: postgresql	# psycopg2: postgresql+psycopg2
+#     user="admin",
+#     password="",
+#     host="localhost",
+#     port="5432",
+#     db_name="admin",
+# )
 
-# 创建数据库引擎,echo为True,会打印所有的sql语句
-engine = create_engine(DB_CONNECT_STRING, echo=True)
+SQLALCHEMY_DATABASE_URI = 'postgresql://:@127.0.0.1:5432/learn'
+DB_CONNECT_STRING = SQLALCHEMY_DATABASE_URI
 
-# 创建一个connection，这里的使用方式与python自带的sqlite的使用方式类似
-with engine.connect() as con:
-    # 执行sql语句，如果是增删改，则直接生效，不需要commit
-    sql_statement = """SELECT * FROM quote_table WHERE id = 1;"""
-    rs = con.execute(sql_statement)
-    data = rs.fetchone()  # fetchone 仅匹配第一条符合
-    print "Data: %s" % data
+# 创建数据库引擎
+engine = create_engine(DB_CONNECT_STRING, echo=True)  # echo为True, 会打印所有的sql语句
+
+
+if __name__ == "__main__":
+
+    # 创建一个connection，这里的使用方式与 python 自带的 sqlite 的使用方式类似
+    with engine.connect() as con:
+
+        # 执行sql语句，如果是增删改，则直接生效，不需要commit
+        sql_statement = """SELECT * FROM users WHERE id = 1;"""
+        rs = con.execute(sql_statement)
+        data = rs.fetchone()  # fetchone 仅匹配第一条符合
+        print "\nData: %s" % data
+
+    # 与 python 自带的 sqlite 不同，这里不需要 Cursor 光标，执行sql语句不需要 commit。
--- learning_record/rec_sqlalchemy/sqlalchemy_tutorial/S02_connection_transaction.py
+++ learning_record/rec_sqlalchemy/sqlalchemy_tutorial/S02_connection_transaction.py
@@ -1,21 +1,23 @@
 # -*- coding: utf-8 -*-
 
 """connection事务
+
     使用事务可以进行批量提交和回滚
 """
 
-from sqlalchemy import create_engine
-from learning_record.rec_sqlalchemy.sqlalchemy_tutorial.S01_connection import DB_CONNECT_STRING
+from learning_record.rec_sqlalchemy.sqlalchemy_tutorial.S01_connection import engine
 
 
-engine = create_engine(DB_CONNECT_STRING, echo=True)
+if __name__ == "__main__":
 
-with engine.connect() as connection:
-    trans = connection.begin()
-    try:
-        r1 = connection.execute("select * from quote_table where 8 <= id and id <= 10")
-        r2 = connection.execute("insert into quote_table(quote, author) values ('how time flies', 'LRW')")
-        trans.commit()
-    except Exception:
-        trans.rollback()
-        raise
+    with engine.connect() as connection:
+        trans = connection.begin()
+        try:
+            r1 = connection.execute("select * from users where 2 <= id and id <= 3")
+            print r1
+            r2 = connection.execute("insert into users(name, fullname, balance) values "
+                                    "('lrw', 'big lrw', 666)")
+            trans.commit()
+        except Exception:
+            trans.rollback()
+            raise
