4215568405b417959e7a935e6ce2f67bedfa7d46
linrenwei <windstreet12@gmail.com>
2019-03-28 10:59:11 +0800

    27

--- learning_record/rec_sqlalchemy/sqlalchemy_tutorial/S07_database_migration.py
+++ learning_record/rec_sqlalchemy/sqlalchemy_tutorial/S07_database_migration.py
@@ -1,24 +1,25 @@
 # -*- coding: utf-8 -*-
 
-"""数据库迁移
+"""数据库迁移 (通过API升级和降级)
 
-sqlalchemy 的数据库 迁移/升级 有两个库支持:
-    1. alembic
-    2. sqlalchemy-migrate【停止维护】
+0. sqlalchemy 的数据库 迁移/升级
 
-alembic 实现了类似 git/svn 的版本管理的控制，我们可以通过 alembic 维护每次升级数据库的版本。
+    有两个库支持:
+        1. alembic
+        2. sqlalchemy-migrate【停止维护】
 
-
+        alembic 实现了类似 git/svn 的版本管理的控制，我们可以通过 alembic 维护每次升级数据库的版本。
 
 1. 安装
 
     $ pip install alembic
+
 2. 初始化
 
     $ cd ~/Desktop/funny/youtube_scraper
     $ alembic init migrations
 
-    alembic 会在根目录创建 migrations 目录和 alembic.ini 文件，如下:
+    2.1 alembic 会在根目录创建 migrations 目录和 alembic.ini 文件，如下:
 
         yourproject/
             alembic.ini     -------------------------> 提供了一些基本的配置（注意要先配置）
@@ -31,15 +32,15 @@ alembic 实现了类似 git/svn 的版本管理的控制，我们可以通过 al
                     2b1ae634e5cd_add_order_id.py
                     3adcc9a56557_rename_username_field.py
 
-    默认情况下创建的是基于单个数据库的。
+    2.2 默认情况下创建的是基于单个数据库的。
 
-3. 配置
+3. 配置 alembic.ini 文件
 
-    使用之前，需要配置一下链接字符串，打开 alembic.ini 文件，设置 sqlalchemy.url 连接字符串，例如:
+    使用之前，需要配置一下数据库链接字符串，例如:
 
         sqlalchemy.url = sqlite:////Users/zhengxiankai/Desktop/database.db
 
-    其他参数可以参见官网说明：http://alembic.zzzcomputing.com/en/latest/tutorial.html
+        参见: http://alembic.zzzcomputing.com/en/latest/tutorial.html
 
 4. 创建数据库版本
 
@@ -48,18 +49,56 @@ alembic 实现了类似 git/svn 的版本管理的控制，我们可以通过 al
         $ alembic revision -m 'create table'
 
         创建一个版本（ 生成 yourproject/migrations/versions/xxx_create_table.py ），
-        该 python 模块包含 upgrade 和 downgrade 两个方法，在这里添加一些新增表的逻辑 (如下代码)。
+        数据库迁移脚本 xxx_create_table.py 包含 upgrade 和 downgrade 两个方法，在这里添加一些新增表的逻辑 (如下代码)。
 
 
-    4.2 这里生成的文件名是依照在 alembic.ini 文件声明的模板来的，默认为: 版本号+名字，
+    4.2 迁移脚本的文件名是在 alembic.ini 文件中声明的。默认为: 版本号+名字，
         可以加上一些日期信息，否则不好排序:
 
             file_template = %%(year)d_%%(month).2d_%%(day).2d_%%(hour).2d_%%(minute).2d_%%(rev)s_%%(slug)s
 
-    4.3 另外通常我们也改一下生成模板 script.py.mako ，加上编码信息，否则在升级脚本中如果有中文会报错。
+    4.3 通常我们需要改一下生成模板 script.py.mako ，加上编码信息（如下），否则在升级脚本中如果有中文会报错。
 
         #!/usr/bin/python
         # -*- coding:utf-8 -*-
+
+5. 升级数据库
+    5.1 上述实现了 升级 和 降级 的方法，通过下面命令升级数据库到最新版本:
+
+        $ alembic upgrade head
+
+        这时候可以看到数据库多了两个表:
+        alembic_version  --->  存放数据库版本
+        account
+
+    5.2 其他命令:
+
+        # 升到最高版本
+        $ alembic upgrade head
+
+        # 降到最初版本
+        $ alembic downgrade base
+
+        # 升两级
+        $ alembic upgrade +2
+
+        # 降一级
+        $ alembic downgrade -1
+
+        # 升级到制定版本
+        $ alembic upgrade e93b8d488143
+
+        # 查看当前版本
+        $ alembic current
+
+        # 查看历史版本详情
+        $ alembic history --verbose
+
+        # 查看历史版本（-r参数）类似切片
+        $ alembic history -r1975ea:ae1027
+        $ alembic history -r-3:current
+        $ alembic history -r1975ea:
+
 
 """
 
